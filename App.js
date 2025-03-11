4f8820317c61e1919ef708ed58a197b4d0ced5df
David A <davidalbertogarciaamaro@gmail.com>
2025-03-11 13:44:52 -0600

    ya actualizado para empezar solo el to do

--- App.js
+++ App.js
@@ -1,45 +1,20 @@
-import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { supabase } from './supabaseClient';
-import LoginScreen from './Login';
 import Main from './Main';
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  const [session, setSession] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const checkSession = async () => {
-      setLoading(true);
-      const { data: { session } } = await supabase.auth.getSession();
-      setSession(session);
-      setLoading(false);
-    };
-
-    checkSession();
-
-    const { subscription } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-
-    return () => subscription?.unsubscribe(); // Limpieza del listener
-  }, []);
-
-  if (loading) {
-    return null; // O puedes mostrar un indicador de carga aquí
-  }
-
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {session ? (
-          <Stack.Screen name="Main" component={Main} />
-        ) : (
-          <Stack.Screen name="Login" component={LoginScreen} />
-        )}
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen 
+          name="Main" 
+          component={Main}
+          options={{
+            headerShown: false // Esto oculta la barra de navegación superior
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
